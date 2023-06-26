3d2b6a4541499bf79d3774711f9c1ec92093e36a
Katharina Gredig <107423444+katharinagredig@users.noreply.github.com>
2023-06-26 17:43:00 +0000

    Hallo Welt Programm hinzugefügt

---
+++ C++/halloWelt.cpp
@@ -0,0 +1,9 @@
+#include <iostream>         // Ein - Ausgabe Stream Bibliothek
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    cout << "This is a new Line" << endl;
+    return 0;
+}
