b655918670c03b7e6783c03d95461dc04c4d5162
Katharina Gredig <107423444+katharinagredig@users.noreply.github.com>
2023-06-30 18:31:44 +0000

    Programm mit Array hinzugefügt

--- C++/Arrays/zahlenInArraySpeichern.cpp
+++ C++/Arrays/zahlenInArraySpeichern.cpp
@@ -2,11 +2,41 @@
 
 using namespace std;
 
-main() {
+int main() {
+
     cout << endl;
     cout << "Dieses Programm speichert 10 Ganzzahlen die vom Nutzer eingegeben werden und gibt diese in umgekehrter Reihnfolge wieder aus.";
     cout << endl;
-    cout << ""
+    
+    int meinArray[10];
 
+    cout << "Bitte geben Sie die erste Zahl ein: " << endl;
+    cin >> meinArray[0];
+    cout << "Bitte geben Sie die zweite Zahl ein: " << endl;
+    cin >> meinArray[1];
+    cout << "Bitte geben Sie die dritte Zahl ein: " << endl;
+    cin >> meinArray[2];
+    cout << "Bitte geben Sie die vierte Zahl ein: " << endl;
+    cin >> meinArray[3];
+    cout << "Bitte geben Sie die fünfte Zahl ein: " << endl;
+    cin >> meinArray[4];
+    cout << "Bitte geben Sie die sechste Zahl ein: " << endl;
+    cin >> meinArray[5];
+    cout << "Bitte geben Sie die siebte Zahl ein: " << endl;
+    cin >> meinArray[6];
+    cout << "Bitte geben Sie die achte Zahl ein: " << endl;
+    cin >> meinArray[7];
+    cout << "Bitte geben Sie die neunte Zahl ein: " << endl;
+    cin >> meinArray[8];
+    cout << "Bitte geben Sie die zehnte Zahl ein: " << endl;
+    cin >> meinArray[9];
 
+    cout << endl;
+
+    cout << "Hier sind die Zahlen in umgekehrter Reihenfolge: " << endl;
+
+    for(int i = 9; i >= 0; i--) {
+        cout << meinArray[i] << " ";
+    }
+    cout << endl;
 }
